320b9626063e8fa27be45fc9a5fb5ebbc5039629
Gregory Palacios <gregpalacios26@gmail.com>
2021-11-25 17:42:11 -0500

    App ingresos y egresos plantilla

---
+++ src/app/ingreso-egreso/estadistica/estadistica.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-estadistica',
+  templateUrl: './estadistica.component.html',
+  styleUrls: ['./estadistica.component.css']
+})
+export class EstadisticaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/ingreso-egreso/ingreso-egreso.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ingreso-egreso',
+  templateUrl: './ingreso-egreso.component.html',
+  styleUrls: ['./ingreso-egreso.component.css']
+})
+export class IngresoEgresoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
